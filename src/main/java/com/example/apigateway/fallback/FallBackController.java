e9c31e51da875be5e36e5918b990ad87d1be5150
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-08-23 00:36:21 -0400

    Adding circuit breaker and resilience to the API Gateway

--- src/main/java/com/example/apigateway/fallback/FallBackController.java
+++ src/main/java/com/example/apigateway/fallback/FallBackController.java
@@ -7,12 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class FallBackController {
     @GetMapping("/securityServiceFallback")
     public String securityFallback(){
-        return "Security service is down!";
+        return "Security service is currently unavailable. Please try again later.";
     }
 
     @GetMapping("/userServiceFallback")
     public String userFallback(){
-        return "User service is down!";
+        return "User service is currently unavailable. Please try again later.";
     }
 
+    @GetMapping("/eurekaServiceFallback")
+    public String eurekaFallback(){
+        return "Eureka service is currently unavailable. Please try again later.";
+    }
 }
